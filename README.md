8e2fa10fda27886f9b20828493b292c7ae95d3cd
Daniel Nunes <danyribeironunes15@gmail.com>
2025-05-25 14:16:58 +0100

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,100 @@
+<br />
+<br />
+<div align="center">
+    <a href="https://github.com/D3W10/SkyShare-WS">
+        <img src="https://raw.githubusercontent.com/D3W10/SkyShare/main/svelte/static/logo.png" alt="Logo" width="60" height="60">
+    </a>
+    <br />
+    <br />
+    <h2 align="center">SkyShare WebSocket Server</h2>
+    <h3 align="center">The WebSocket server for SkyShare</h3>
+    <br />
+    <p align="center">
+        <a href="https://github.com/D3W10/SkyShare-WS/issues">Report Bug</a>
+        ·
+        <a href="https://github.com/D3W10/SkyShare-WS/issues">Request Feature</a>
+    </p>
+</div>
+<br />
+
+### Table of Contents
+1. [About](#about)
+    - [Built With](#built-with)
+2. [Development](#development)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+3. [License](#license)
+4. [Credits](#credits)
+
+<br />
+<br />
+
+## About
+
+SkyShare WebSocket Server is the bridge that allows 2 peers to exchange their WebRTC signals, helping in establishing a connection between them.
+
+This project consists in four different subprojects:
+> 🖥️&emsp;SkyShare [Desktop App](https://github.com/D3W10/SkyShare)
+>
+> 🔌&emsp;SkyShare [API](https://github.com/D3W10/SkyShare-API)
+>
+> 🌐&emsp;SkyShare [Website](https://github.com/D3W10/SkyShare-Website)
+>
+> 📩&emsp;SkyShare [Mail](https://github.com/D3W10/SkyShare-Mail)
+>
+> 🔗&emsp;SkyShare [WebSocket Server](https://github.com/D3W10/SkyShare-WS)
+
+<br />
+
+### Built With
+
+- [TypeScript](https://www.typescriptlang.org/)
+- WebSockets
+
+<br />
+<br />
+
+## Development
+
+If you want to deploy a copy of SkyShare WebSocket Server on your device to develop a feature or fix a bug, follow the steps below to get started.
+
+<br />
+
+### Prerequisites
+
+In order to run the application, you will need the following tools:
+- Node.JS (`20.0.0` or higher);
+- bun (or equivalent);
+- git (*optional*).
+
+<br />
+
+### Installation
+
+1. Clone the repository
+    ```sh
+    git clone https://github.com/D3W10/SkyShare-WS.git
+    ```
+2. Open the project folder using your prefered code editor (ex: VS Code)
+3. Install the required dependencies on both the backend and frontend
+    ```sh
+    bun i
+    ```
+4. On the project root run the following command to run the server
+    ```sh
+    bun run dev
+    ```
+
+<br />
+<br />
+
+## License
+
+Distributed under the Mozilla Public License 2.0. Check `LICENSE` for more details.
+
+<br />
+<br />
+
+## Credits
+
+- Made by [Daniel Nunes](https://d3w10.netlify.app/)
---
+++ index.ts
@@ -0,0 +1,181 @@
+import { WebSocketServer } from "ws";
+import { Client, type Notification } from "pg";
+import type { Message } from "./models/Message.interface";
+
+const server = new WebSocketServer({
+    port: 8082
+});
+
+const generateCode = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL
+});
+await client.connect();
+
+function parseMsg(msg: Notification) {
+    const code = msg.channel;
+    const payload = msg.payload ? JSON.parse(msg.payload) : null;
+
+    return { code, payload };
+}
+
+server.on("connection", socket => {
+    let code = "", listener: (msg: Notification) => unknown;
+
+    console.log("Client connected");
+
+    async function cleanConnection() {
+        if (code)
+            await client.query(`UNLISTEN "${code}"`);
+        client.off("notification", listener);
+    }
+
+    socket.on("message", async message => {
+        let parsed: Message;
+
+        try {
+            parsed = JSON.parse(message.toString());
+        }
+        catch (err) {
+            console.error("Invalid JSON received");
+            socket.send(JSON.stringify({ type: "error", error: "invalidData" }));
+            socket.close(1000, "Invalid JSON");
+            return;
+        }
+
+        const { type, data } = parsed;
+        if (!type || !data) {
+            socket.send(JSON.stringify({ type: "error", error: "typeMissing" }));
+            socket.close(1000, "Missing type or data.");
+            return;
+        }
+
+        if (type === "offer") {
+            const offer = data.offer;
+            code = generateCode();
+
+            socket.send(JSON.stringify({ type: "code", code }));
+            await client.query(`LISTEN "${code}"`);
+
+            const timeout = setTimeout(async () => {
+                console.log(`Timeout reached for code ${code}, closing LISTEN`);
+                cleanConnection();
+                socket.send(JSON.stringify({ type: "error", error: "timeout" }));
+                socket.close(1000, "Timeout reached.");
+            }, 600000);
+
+            listener = async (msg: Notification) => {
+                if (!msg.payload || msg.channel !== code)
+                    return;
+            
+                const { payload } = parseMsg(msg);
+
+                if (payload.type === "ping")
+                    client.query(`NOTIFY "${code}", '${JSON.stringify({ type: "echo" })}'`);
+                else if (payload.type === "getOffer") {
+                    client.query(`NOTIFY "${code}", '${JSON.stringify({ type: "offer", data: { offer } })}'`);
+                    clearTimeout(timeout);
+                }
+                else if (payload.type === "setAnswer")
+                    socket.send(JSON.stringify({ type: "answer", data: payload.data }));
+                else if (payload.type === "iceReceiver")
+                    socket.send(JSON.stringify({ type: "iceReceiver", data: payload.data }));
+            }
+
+            client.on("notification", listener);
+        }
+        else if (type === "connect") {
+            code = data.code;
+            if (!code) {
+                socket.send(JSON.stringify({ type: "error", error: "invalidCode" }));
+                socket.close(1000, "Missing code");
+                return;
+            }
+
+            await client.query(`LISTEN "${code}"`);
+
+            const timeout = setTimeout(() => {
+                socket.send(JSON.stringify({ type: "error", error: "invalidCode" }));
+                socket.close(1000, "Invalid code");
+            }, 4000);
+
+            listener = async (msg: Notification) => {
+                if (!msg.payload || msg.channel !== code)
+                    return;
+            
+                const { payload } = parseMsg(msg);
+
+                if (payload.type === "echo") {
+                    client.query(`NOTIFY "${code}", '${JSON.stringify({ type: "getOffer" })}'`);
+                    clearTimeout(timeout);
+                }
+                else if (payload.type === "offer") {
+                    socket.send(JSON.stringify({ type: "offer", data: payload.data }));
+                    cleanConnection();
+                }
+            }
+
+            client.on("notification", listener);
+            client.query(`NOTIFY "${code}", '${JSON.stringify({ type: "ping" })}'`);
+        }
+        else if (type === "answer") {
+            const answer = data.answer;
+            code = data.code;
+            if (!code) {
+                socket.send(JSON.stringify({ type: "error", error: "invalidCode" }));
+                socket.close(1000, "Missing code");
+                return;
+            }
+
+            await client.query(`LISTEN "${code}"`);
+
+            listener = async (msg: Notification) => {
+                if (!msg.payload || msg.channel !== code)
+                    return;
+            
+                const { payload } = parseMsg(msg);
+
+                if (payload.type === "iceSender")
+                    socket.send(JSON.stringify({ type: "ice", data: payload.data }));
+            }
+
+            client.on("notification", listener);
+            client.query(`NOTIFY "${code}", '${JSON.stringify({ type: "setAnswer", data: { answer } })}'`);
+        }
+        else if (type === "iceSender") {
+            const ice = data.ice;
+            code = data.code;
+            if (!code) {
+                socket.send(JSON.stringify({ type: "error", error: "invalidCode" }));
+                socket.close(1000, "Missing code");
+                return;
+            }
+
+            client.query(`NOTIFY "${code}", '${JSON.stringify({ type: "iceSender", data: { ice } })}'`);
+        }
+        else if (type === "iceReceiver") {
+            const ice = data.ice;
+            code = data.code;
+            if (!code) {
+                socket.send(JSON.stringify({ type: "error", error: "invalidCode" }));
+                socket.close(1000, "Missing code");
+                return;
+            }
+
+            client.query(`NOTIFY "${code}", '${JSON.stringify({ type: "iceReceiver", data: { ice } })}'`);
+        }
+        else {
+            socket.send(JSON.stringify({ type: "error", error: "unknownType" }));
+            socket.close(1000, "Unknown message type.")
+        }
+    });
+
+    socket.on("close", async () => {
+        console.log("Client disconnected");
+
+        cleanConnection();
+    });
+});
+
+console.log("Server started.");
